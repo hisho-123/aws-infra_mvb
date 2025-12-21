d09d677057d020e5b81d3ef2d71fd70f60a7efb9
hisho-123 <vahcp92774@yahoo.co.jp>
2025-12-21 17:35:00 +0900

    3層アーキテクチャcloud版の構築

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+project_name = "my-vocaburary-book"
+vpc_cidr     = "10.0.0.0/16"
+public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnet_cidrs = ["10.0.10.0/24", "10.0.11.0/24"]
+availability_zones   = ["ap-northeast-1a", "ap-northeast-1c"]
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "mvb"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["ap-northeast-1a", "ap-northeast-1c"]
+}
